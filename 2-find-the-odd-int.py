896a944f3287a9cc70cd9090d1ada88743f9b580
katherine-kania <katherine.kania@gmail.com>
2022-04-14 11:37:01 -0400

    fine the odd solved

--- 2-find-the-odd-int.py
+++ 2-find-the-odd-int.py
@@ -24,4 +24,21 @@ Output: 10
 '''    
 
 def find_it(li):
-  pass
+
+  hash_table = {}
+  
+  for num in li:
+    if num not in hash_table:
+      hash_table[num] = 1
+    else:
+      hash_table[num] = hash_table[num] + 1
+
+  for num in hash_table:
+    if hash_table[num] % 2 != 0:
+      print(num)
+
+find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
+find_it([1,1,2,-2,5,2,4,4,-1,-2,5])
+find_it([10])
+
+
